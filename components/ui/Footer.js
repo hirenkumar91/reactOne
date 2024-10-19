e95ad5f33bda9bd3c220001716bcfd12841f929d
hirenkumar91 <123118011+hirenkumar91@users.noreply.github.com>
2024-10-19 16:39:12 +0000

    code formated

--- components/ui/Footer.js
+++ components/ui/Footer.js
@@ -1,55 +1,73 @@
-"use client"
+"use client";
 
-import { usePathname } from 'next/navigation';
-import styles from './Footer.module.css';
-import Link from 'next/link';
-import data from '@/data/galectica.json';
-import SocialmediaItem from './socialMediaItem';
-import { faLinkedin, faFacebook, faInstagram, faTiktok, faGoogle } from '@fortawesome/free-brands-svg-icons';
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { usePathname } from "next/navigation";
+import styles from "./Footer.module.css";
+import Link from "next/link";
+import data from "@/data/galectica.json";
+import SocialmediaItem from "./socialMediaItem";
+import {
+  faLinkedin,
+  faFacebook,
+  faInstagram,
+  faTiktok,
+  faGoogle,
+} from "@fortawesome/free-brands-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
 
 export const Footer = () => {
-  const path = usePathname().split('?')[0];
+  const path = usePathname().split("?")[0];
   return (
     <footer className={path !== "/" ? styles.footer : styles.hidden}>
       <div className={styles.footerDescription}>
         <h3>Galactica</h3>
-        <p>Explore the universe and beyond. Your journey to the stars starts here.</p>
+        <p>
+          Explore the universe and beyond. Your journey to the stars starts
+          here.
+        </p>
         <p>&copy; 2024 Galactica. All rights reserved.</p>
       </div>
       <div className={styles.footerLinks}>
         <h3>Pages</h3>
         <ul className={styles.footerList}>
-          <li> <Link href="/">Home</Link></li>
-          <li><Link href="/about_us">About Us</Link></li>
-          <li><Link href="/destination">Destination</Link></li>
-          <li><Link href="/nasa_">NASA Collaboration</Link></li>
+          <li>
+            {" "}
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about_us">About Us</Link>
+          </li>
+          <li>
+            <Link href="/destination">Destination</Link>
+          </li>
+          <li>
+            <Link href="/nasa_">NASA Collaboration</Link>
+          </li>
         </ul>
       </div>
       {/* Docs for the Link: https://nextjs.org/docs/pages/api-reference/components/link */}
-      
+
       <div className={styles.footerLinks}>
         <h3>Follow us</h3>
         <ul className={styles.footerList}>
-        {data.socialMediaLinks.map((socialMedia, index) => {
+          {data.socialMediaLinks.map((socialMedia, index) => {
             const iconMap = {
-              'fab fa-linkedin': faLinkedin,
-              'fab fa-facebook': faFacebook,
-              'fab fa-instagram': faInstagram,
-              'fab fa-tiktok': faTiktok,
-              'fab fa-google': faGoogle,
+              "fab fa-linkedin": faLinkedin,
+              "fab fa-facebook": faFacebook,
+              "fab fa-instagram": faInstagram,
+              "fab fa-tiktok": faTiktok,
+              "fab fa-google": faGoogle,
             };
-        return (
-          <SocialmediaItem
-            key={index}
-            url={socialMedia.url}
-            title={socialMedia.title}
-            icon={iconMap[socialMedia.icon]}
-          />
-        );
-      })}
+            return (
+              <SocialmediaItem
+                key={index}
+                url={socialMedia.url}
+                title={socialMedia.title}
+                icon={iconMap[socialMedia.icon]}
+              />
+            );
+          })}
         </ul>
       </div>
     </footer>
   );
-}
+};
--- components/ui/Navbar.js
+++ components/ui/Navbar.js
@@ -1,39 +1,37 @@
-"use client"
-import classNames from 'classnames';
-import { usePathname } from 'next/navigation';
-import styles from './Navbar.module.css';
-import Navitem from './navItem';
-import data from '@/data/galectica.json';
-
+"use client";
+import classNames from "classnames";
+import { usePathname } from "next/navigation";
+import styles from "./Navbar.module.css";
+import Navitem from "./navItem";
+import data from "@/data/galectica.json";
 
 export const Navbar = () => {
-  const currentPath = usePathname()
+  const currentPath = usePathname();
 
   return (
     <header className={styles.headerContainer}>
       <div className={styles.navbarLogo}>
-        <a href="/"><img src="/shared/logo.svg" alt="" /> GALACTICA</a>
+        <a href="/">
+          <img src="/shared/logo.svg" alt="" /> GALACTICA
+        </a>
       </div>
       <div className={styles.decorativeLine} />
       <nav className={styles.navbar}>
         <div className={styles.navbarBG} />
         <ul className={styles.navbarList}>
-          {
-            data.navbarItems.map((navItem, index) => {
-              const isActive = navItem.link === currentPath;
-              return (
-                <Navitem
-                  key={index}
-                  title={navItem.title}
-                  link={navItem.link}
-                  isActive={isActive}
-                />
-              )
-            }
-            )
-          }
+          {data.navbarItems.map((navItem, index) => {
+            const isActive = navItem.link === currentPath;
+            return (
+              <Navitem
+                key={index}
+                title={navItem.title}
+                link={navItem.link}
+                isActive={isActive}
+              />
+            );
+          })}
         </ul>
       </nav>
     </header>
   );
-}
+};
