f1be0404a51a4d3ae9336d5347542669a5d7e44d
hirenkumar91 <123118011+hirenkumar91@users.noreply.github.com>
2024-10-19 10:22:17 +0000

    create file PlanetWishlistItem.js

---
+++ components/destination/planetWishlistItem.js
@@ -0,0 +1,20 @@
+"use client";
+
+import styles from '@/components/destination/destination.module.css';
+
+// TASK - React 1 week 2
+// Move this to its own file
+const PlanetWishlistItem = ({
+    name,
+    onRemove,
+    thumbnail,
+  }) => {
+    return (
+      <div className={styles.wishlistItem}>
+        <img className={styles.wishlistItemThumbnail} src={thumbnail} alt="" />
+        <b>{name.toUpperCase()}</b>
+        <button onClick={onRemove}>remove</button>
+      </div>
+    );
+  }
+  
